66d23a59d847c53eb168124a97966df98ac027fd
Victor Oviedo <drvictoroviedo@gmail.com>
2020-07-28 12:12:44 -0400

    RecyclerView

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.recyclerview
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.squareup.okhttp.Dispatcher
+import kotlin.coroutines.CoroutineContext
 
 lateinit var mRecyclerView : RecyclerView
 val mAdapter : RecyclerAdapter = RecyclerAdapter()
+
+var db = FirebaseFirestore.getInstance()
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +29,23 @@ class MainActivity : AppCompatActivity() {
         mRecyclerView.adapter = mAdapter
     }
 
+    /*
+    fun getSuperheros(): MutableList<Superhero>{
+    var superheros:MutableList<Superhero> = ArrayList()
+    var sh:String;var ed:String;var rn:String;var im:String;
+    db.collection("SuperHeroes")
+    .get()
+    .addOnSuccessListener { result ->
+    for (document in result) {
+    sh=document.data["superheroe"] as String
+    ed=document.data["Editorial"] as String
+    rn=document.data["RealName"] as String
+    im=document.data["imagen"] as String
+    }}
+    return superheros
+    }
+    */
+
     fun getSuperheros(): MutableList<Superhero>{
         var superheros:MutableList<Superhero> = ArrayList()
         superheros.add(Superhero("Spiderman", "Marvel", "Peter Parker", "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg"))
@@ -36,5 +57,8 @@ class MainActivity : AppCompatActivity() {
         superheros.add(Superhero("Green Lantern", "DC", "Alan Scott", "https://cursokotlin.com/wp-content/uploads/2017/07/green-lantern.jpg"))
         superheros.add(Superhero("Wonder Woman", "DC", "Princess Diana", "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"))
         return superheros
-    }
+   }
+
 }
+
+
